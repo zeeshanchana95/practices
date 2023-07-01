44a4b544bd86a372e5d3e12fac20d9465c20b8ae
zeeshanchana95 <zeeshanchana95@gmail.com>
2023-07-01 13:54:09 +0500

    add react practice and lab files

---
+++ react/practice01/Lab03_Passing props/src/App.js
@@ -0,0 +1,17 @@
+
+export default App;
+
+
+
+import Heading from "./Heading";
+
+function App() {
+    return (
+        <div className="App">
+			<Heading firstName="Any name other than Zeeshan" />
+			<Heading firstName="Bob" />
+        </div>
+    );
+};
+
+export default App;
